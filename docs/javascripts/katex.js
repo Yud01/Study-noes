94467f966600ea11c1ccdfa87b6f445601f83401
Yudong Qiu <qiuyudong4@gmail.com>
2025-09-08 16:30:43 +1000

    lagtm

---
+++ docs/javascripts/katex.js
@@ -0,0 +1,13 @@
+document$.subscribe(() => {
+  if (window.renderMathInElement) {
+    renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$",  right: "$",  display: false },
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true }
+      ],
+      throwOnError: false
+    });
+  }
+});
